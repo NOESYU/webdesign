abbb10690e09fe54f9fb65d6bfa4cc9a867a20ef
Jeon. <86655940+NOESYU@users.noreply.github.com>
2024-07-31 13:00:52 +0900

    add E4

---
+++ E4/js/script.js
@@ -0,0 +1,15 @@
+$(".submenu").hide();
+$(".menu>li").mouseover(function(){
+  $(this).children(".submenu").stop().fadeIn();
+})
+$(".menu>li").mouseout(function(){
+  $(this).children(".submenu").stop().fadeOut();
+})
+
+
+setInterval(slide);
+function slide(){
+  $(".slide").delay(2000).animate({marginLeft: "-100%"}, 1000);
+  $(".slide").delay(2000).animate({marginLeft: "-200%"}, 1000);
+  $(".slide").delay(2000).animate({marginLeft: "0%"}, 1000);
+}
